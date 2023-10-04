b9d4aeead19190be83c40673f3471cea906f195b
mohamedelshwimy <mohamedahmedebrahim97@gmail.com>
2023-10-04 07:50:50 +0300

    Finished the course

--- src/test/java/Base/BaseTests.java
+++ src/test/java/Base/BaseTests.java
@@ -1,11 +1,19 @@
 package Base;
 
 import Utils.WindowManager;
+import com.google.common.io.Files;
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
 import org.testng.annotations.*;
 import pages.HomePage;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class BaseTests {
@@ -14,10 +22,17 @@ public class BaseTests {
 
     @BeforeClass
     public void setUp(){
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(getChromeOptions());
         goToHomePage();
+        //setCookies();
         driver.manage().window().maximize();
         homePage = new HomePage(driver);
+
+        //In case of Event Reporter
+        /*
+        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver.register(new EventReporter());
+         */
     }
     @BeforeMethod
     public void goToHomePage(){
@@ -27,7 +42,33 @@ public class BaseTests {
     public void tearDown(){
         driver.quit();
     }
+    @AfterMethod
+    public void recordFailure(ITestResult result){
+        if(ITestResult.FAILURE == result.getStatus()) {
+            var camera = (TakesScreenshot) driver;
+            File screenShot = camera.getScreenshotAs(OutputType.FILE);
+            try {
+                Files.move(screenShot, new File("resources/ScreenShots/"+ result.getName() + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     public WindowManager getWindowManager(){
         return new WindowManager(driver);
+    }
+
+    private ChromeOptions getChromeOptions(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        return options;
+    }
+    private void setCookies(){
+        Cookie cookie = new Cookie
+                .Builder("tau", "123")
+                .domain("https://the-internet.herokuapp.com/")
+                .build();
+        driver.manage().addCookie(cookie);
     }
 }
